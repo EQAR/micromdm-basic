ad93393197afcbf2a11238e31ebd12f7b74911b9
Colin Tück <colin.tueck@eqar.eu>
2024-05-09 15:13:29 +0200

    webhook: implement InstallProfile command

--- webhook/app.py
+++ webhook/app.py
@@ -10,6 +10,28 @@ import logging
 
 from urllib.parse import urljoin
 
+class MicroMdmSession(requests.Session):
+    """
+    Requests Session for talking to the MicroMDM API
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.auth = requests.auth.HTTPBasicAuth('micromdm', config['API_KEY'])
+        adapter = requests.adapters.HTTPAdapter(max_retries=config.get("API_RETRY", 5))
+        self.mount('http://', adapter)
+        self.mount('https://', adapter)
+        self.default_timeout = config.get("API_TIMEOUT", 30)
+        self.base_url = config.get("API_URL", 30)
+
+    def post(self, path, *args, **kwargs):
+        """
+        apply default timeout and prepend base URL
+        """
+        if 'timeout' not in kwargs:
+            kwargs['timeout'] = self.default_timeout
+        return super().post(urljoin(self.base_url, path), *args, **kwargs)
+
 
 def create_app(test_config=None):
     """
@@ -22,12 +44,11 @@ def create_app(test_config=None):
     app.config.from_prefixed_env(prefix='MICROMDM')
 
     # set up a session for requests to MicroMDM API
-    app.req_session = requests.Session()
-    app.req_session.auth = requests.auth.HTTPBasicAuth('micromdm', app.config['API_KEY'])
+    app.req_session = MicroMdmSession(app.config)
 
     # fetch list of blueprints and profile IDs contained
     app.profiles = dict()
-    r = app.req_session.post(urljoin(app.config['API_URL'], '/v1/blueprints'), json={})
+    r = app.req_session.post('/v1/blueprints', json={})
     for blueprint in r.json()['blueprints']:
         app.logger.info(f'blueprint {blueprint["uuid"]} [{blueprint["name"]}] lists the following profiles:')
         for profile in blueprint["profile_ids"]:
@@ -37,7 +58,7 @@ def create_app(test_config=None):
     # fetch profiles to get the expected UUIDs
     app.logger.info('profile details:')
     for profile_id in app.profiles.keys():
-        r = app.req_session.post(urljoin(app.config['API_URL'], '/v1/profiles'), json={ "id": profile_id })
+        r = app.req_session.post('/v1/profiles', json={ "id": profile_id })
         for profile in r.json()['profiles']:
             if profile['Identifier'] == profile_id:
                 payload = plistlib.loads(base64.b64decode(profile['Mobileconfig']))
@@ -55,17 +76,26 @@ def create_app(test_config=None):
         if 'acknowledge_event' in request.json and 'raw_payload' in request.json['acknowledge_event']:
             plist = base64.b64decode(request.json['acknowledge_event']['raw_payload'])
             payload = plistlib.loads(plist)
+            udid = payload["UDID"]
             if 'ProfileList' in payload:
-                app.logger.info(f'checking ProfileList from {payload["UDID"]}:')
+                app.logger.info(f'checking ProfileList from {udid}:')
                 installed = { i["PayloadIdentifier"]: i["PayloadUUID"] for i in payload["ProfileList"] }
                 for profile_id in app.profiles.keys():
                     if profile_id in installed:
                         if installed[profile_id] == app.profiles[profile_id]['uuid']:
-                            app.logger.info(f' - {profile_id} already installed, UUID is correct')
+                            app.logger.info(f' - {profile_id} already installed, UUID correct')
+                            continue
                         else:
-                            app.logger.info(f' - {profile_id} already installed, UUID mismatch: {installed[profile_id]} [installed] != {app.profiles[profile_id]["uuid"]} [required]')
+                            app.logger.warning(f' - {profile_id} already installed, UUID mismatch: {installed[profile_id]} [installed] != {app.profiles[profile_id]["uuid"]} [required]')
                     else:
-                        app.logger.info(f' - {profile_id} is not installed')
+                        app.logger.warning(f' - {profile_id} is not installed')
+                    # install
+                    app.req_session.post('/v1/commands', json={
+                        "request_type": 'InstallProfile',
+                        "udid": udid,
+                        "payload": app.profiles[profile_id]['payload'],
+                    })
+                    app.logger.info(f' - queued InstallProfile command for {profile_id} on {udid}')
             else:
                 app.logger.debug(f'response from {payload["UDID"]} is not a ProfileList, ignoring')
             return ('', 204)
